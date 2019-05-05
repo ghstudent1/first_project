2c177bf8f264a357d2c4dc4bf26d7a5a39cae9dc
ghstudent1 <49254550+ghstudent1@users.noreply.github.com>
2019-05-05 18:22:31 +0100

    change in flask-hello add SCM

--- flask_hello.py
+++ flask_hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! test build works using pipeline!!!!"
+    return "Hello World! test build works using pipeline SCM!!!!"
 
 if __name__ == "__main__":
     app.run()
