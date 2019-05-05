296e12ffbd3a6426f21d31196c8d66f1e1b36b18
ghstudent1 <ghstudent@gmail.com>
2019-05-05 15:42:57 +0100

    add changes in flask_hello.py v0.2 pipeline

--- flask_hello.py
+++ flask_hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! test build works!!!!"
+    return "Hello World! test build works using pipeline!!!!"
 
 if __name__ == "__main__":
     app.run()
